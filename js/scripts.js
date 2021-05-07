0234ac4f6fc46affd4f9e68e613ba64943618d48
StragelX <stragelx@gmail.com>
2021-05-07 20:50:07 +0300

    f

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 $( document ).ready(function() {
-    $('menu:not(.active)').click(function(event){
-        $(this).addClass('active');
-        event.stopPropagation();
+    $('.open_close_menu').click(function(){
+        $('menu').toggleClass('active');
+        $('body').toggleClass('no_scroll');
     });
 
-    $('.menu_close_bar').click(function(event){
-        $('menu').removeClass('active');
-        event.stopPropagation();
+    $('menu .line.with_group').click(function(){
+        var id = $(this).attr('data-id');
+
+        $('menu .links').each(function(){
+            $(this).hide();
+            if( $(this).attr("data-id") == id ) {
+                $(this).show();
+            }
+        })
     });
+
+    $('.cookies_wrap .btn_transparent').click(function(){
+        $(this).closest('.cookies_wrap').removeClass('active');
+    })
+
+    $('.lang ').click(function(){
+        $(this).toggleClass('active');
+    })
 
     if($('.counter-number').length){
         $('.counter-number').appear(function () {
@@ -38,15 +52,29 @@ $( document ).ready(function() {
             },
         });
     }
+
+    if ($('.suuces_story_swiper')) {
+        const suuces_story_swiper = new Swiper('.suuces_story_swiper', {
+            slidesPerView: 3,
+            spaceBetween: 120,
+            loop: true,
+            autoplay: true
+        });
+    }
     
     if ($('.clients_logos_swiper')) {
         const clients_logos_swiper = new Swiper('.clients_logos_swiper', {
             slidesPerView: 6,
             loop: true,
-            autoplay: {
-              delay: 500,
-              disableOnInteraction: false,
-            },
+            autoplay: true
+        });
+    }
+
+    if ($('.sertificates_swiper')) {
+        const sertificates_swiper = new Swiper('.sertificates_swiper', {
+            slidesPerView: 4,
+            loop: true,
+            autoplay: true
         });
     }
 });
